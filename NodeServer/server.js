07113daec4a3f34a5c6f6981b278f9878dc6305e
mcd-50 <ayush.as.shukla@gmail.com>
2017-10-07 23:01:16 +0530

    Added es6 and chat server

--- NodeServer/server.js
+++ NodeServer/server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const app = express();
+const _http = require('http').Server(app);
+const io = require('socket.io')(_http);
 const cors = require('cors');
 const request = require('request');
 const cheerio = require('cheerio');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
-
 const BASE = "https://search.wn.com/";
 const ACTION = "&action=search&results_type=news&sort_type=-pub-datetime";
-
+const CONNECTION = 'connection';
+const MESSAGE = 'message';
+const JOIN = 'join';
+const LEAVE = 'leave';
+const JOINED = 'joined';
+const LEFT = 'left';
+const ERROR = 'error';
 
 const language_id = {
 	"bengali": 42, "chinese": 36, "dutch": 18, "english": 1, "french": 5, "german": 6, "gujarati": 41,
@@ -20,10 +27,9 @@ const language_id = {
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.use(cors());
 
-app.listen(process.env.PORT || 2000);
+_http.listen(process.env.PORT || 2000);
 
 app.get('/', (req, res) => {
 	res.json({
@@ -31,15 +37,51 @@ app.get('/', (req, res) => {
 	});
 });
 
+io.on(CONNECTION, (socket) => {
+
+	socket.on(JOIN, (room_name) => {
+		socket.join(room_name);
+		io.emit(JOINED, "Joined");
+	});
+
+	socket.on(LEAVE, (room_name) => {
+		socket.leave(room_name);
+		io.emit(LEFT, "Left");
+	});
+
+	socket.on(MESSAGE, (message) => {
+		console.log(typeof message != 'object');
+		if (message && typeof message != 'object') {
+			return io.emit(MESSAGE, { text: message });
+			return;
+		}
+
+		if (message && typeof message == 'object') {
+			return getNews(message, (items) => {
+				io.emit(MESSAGE, JSON.stringify(items));
+			});
+		}
+
+		io.emit(MESSAGE, { text: "An error occured try in a bit." });
+
+	});
+});
+
 
 app.post('/scrape', function (req, res) {
 	//All the web scraping magic will happen here
-	const body = req.body;
-	//build query
-	const pagenum = "?pagenum=" + body.page_number || "?pagenum=1";
-	const action = ACTION;
-	let search_string = "&search_string=" + body.search_string || "top news";
-	const language_id = "&language_id=" + get_lang_id(body.language_name)
+	getNews(req.body, (items) => {
+		res.json(prepareResponse(items));
+	});
+})
+
+
+const getNews = (body, callback) => {
+	let pagenum, accept = ACTION, search_string, language_id;
+	pagenum = body && body.page_number && "?pagenum=" + body.page_number || "?pagenum=1";
+	action = ACTION;
+	search_string = body && body.search_string && "&search_string=" + body.search_string || "top news";
+	language_id = body && body.language_name && "&language_id=" + getLangId(body.language_name) || 1;
 
 	//request query
 	const request_url = BASE + pagenum + action + search_string + language_id;
@@ -51,7 +93,6 @@ app.post('/scrape', function (req, res) {
 		if (!error && response.statusCode == 200) {
 			// Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
 			const $html = cheerio.load(response.body);
-
 			$html("div.news-story").each(function (i, e) {
 				const title = $html(this).children('div.title').children('a.healine').children().text()
 				const author = $html(this).children('div.title').next().text();
@@ -59,29 +100,28 @@ app.post('/scrape', function (req, res) {
 				const description = $html(this).children('div.summary').children().text();
 				const url = $html(this).children('div.title').next().attr('href')
 				items.push({
-					title: title, 
+					title: title,
 					author: author,
 					published_at: published_at,
 					description: description,
 					url: url
 				});
 			});
-			res.json(prepare_response(items));
+			callback(items);
 		} else {
-			res.json(prepare_response(items));
+			callback(items);
 		}
 	})
+}
 
-})
-
-const get_lang_id = (lang) => {
+const getLangId = (lang) => {
 	if (!lang) return 1
 	lang = lang.toLowerCase().trim();
 	const id = language_id[lang] || 1
 	return id;
 }
 
-const prepare_response = (items = []) => {
+const prepareResponse = (items = []) => {
 	return { "items": items };
 }
 
